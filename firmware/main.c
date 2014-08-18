f4d8b8943cc09bcc06ddbf5eaa79984754f38ae8
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2014-08-19 01:01:23 +0200

    Added CLKOUT configuration for debugging

--- firmware/main.c
+++ firmware/main.c
@@ -2,14 +2,23 @@
 #include "timer16/timer16.h"
 #include "gpio/gpio.h"
 #include "uart/uart.h"
+#include "lpc134x.h"
 
 volatile int timerCount = 0;
 volatile int idx = -9;
 
 int main() {
-    int i,j;
+    int i;
 
 	cpuInit();
+
+    // main clock to CLKOUT pin
+    SCB_CLKOUTCLKSEL = 0x3;
+    SCB_CLKOUTCLKUEN = SCB_CLKOUTCLKUEN_UPDATE;
+    SCB_CLKOUTCLKUEN = SCB_CLKOUTCLKUEN_DISABLE;
+    SCB_CLKOUTCLKUEN = SCB_CLKOUTCLKUEN_UPDATE;
+    while ( !(SCB_CLKOUTCLKUEN & 0x01) );
+    SCB_CLKOUTCLKDIV = SCB_CLKOUTCLKDIV_DIV1;
 
     uartInit(31250);
 
