122e408d9937c4939a1ab200afeefd7cb2ce6477
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2014-08-18 23:26:57 +0200

    Temporary code - testing MIDI out

--- firmware/main.c
+++ firmware/main.c
@@ -1,17 +1,25 @@
 #include "cpu/cpu.h"
 #include "timer16/timer16.h"
 #include "gpio/gpio.h"
+#include "uart/uart.h"
 
 volatile int timerCount = 0;
 volatile int idx = -9;
 
 int main() {
+    int i,j;
+
 	cpuInit();
 
-	timer16Init(0, TIMER16_CCLK_100US);
-	timer16Enable(0);
+    uartInit(31250);
 
-	while (1) {
+    while (1) {
+        //uartSend((uint8_t*) "\x91\x11\x40\x81\x11\x40", 6);
+        uartSend((uint8_t*) "\x91\x11\x40", 3);
+
+        for (i = 0; i < 10000; i++) {
+             __asm volatile ("NOP");
+        }
 	}
 
 	return 0;
