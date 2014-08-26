b324463fa7a32cbec4801a8b1f32624edc96565a
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2014-08-26 17:55:15 +0200

    MIDI FIFO code made interrupt-safe

--- firmware/midi.c
+++ firmware/midi.c
@@ -4,16 +4,16 @@
 #define BUFFER_SIZE     (32)
 
 // FIFO buffer
-static uint8_t fifo[BUFFER_SIZE];
+static volatile uint8_t fifo[BUFFER_SIZE];
 
-static unsigned int fifoWr = 0;
-static unsigned int fifoRd = 0;
-static unsigned int fifoCount = 0;
+static volatile unsigned int fifoWr = 0;
+static volatile unsigned int fifoRd = 0;
 
 #define NOTE_ON     (0x90)
 #define NOTE_OFF    (0x80)
 
 static bool MIDI_QueueNoteMsg(unsigned int msg, unsigned int note, unsigned int velocity);
+static inline void MIDI_QueueByte(uint8_t b);
 
 void MIDI_Init()
 {
@@ -32,31 +32,31 @@ void MIDI_QueueNoteOff(unsigned int channel, unsigned int note, unsigned int vel
 
 void MIDI_SendQueued()
 {
-    // TODO: take care of concurrency
-    while (fifoCount > 0) {
-        // Note: this part is the only code being executed outside of the interrupt context,
-        // so we can do it in a blocking manner
-        uartSendByte(fifo[fifoRd++]);
-        fifoRd %= BUFFER_SIZE;
-        fifoCount--;
+    // Note: we use the "one slot unused" FIFO variant, and thus don't need to disable
+    // interrupts here
+    while (fifoRd != fifoWr) {
+        uartSendByte(fifo[fifoRd]);
+        fifoRd = (fifoRd + 1) % BUFFER_SIZE;
     }
 }
 
 static bool MIDI_QueueNoteMsg(unsigned int msg, unsigned int note, unsigned int velocity)
 {
-    if (fifoCount + 3 > BUFFER_SIZE)
+    // Avoid puting partial message in the queue - check if there are 3 bytes left
+    int count = (fifoWr + BUFFER_SIZE - fifoRd) % BUFFER_SIZE;
+    if (count + 3 > BUFFER_SIZE)
         return false;
 
-    fifo[fifoWr++] = msg;
-    fifoWr %= BUFFER_SIZE;
-
-    fifo[fifoWr++] = note;
-    fifoWr %= BUFFER_SIZE;
-
-    fifo[fifoWr++] = velocity;
-    fifoWr %= BUFFER_SIZE;
-
-    fifoCount += 3;
+    MIDI_QueueByte(msg);
+    MIDI_QueueByte(note);
+    MIDI_QueueByte(velocity);
 
     return true;
 }
+
+static inline void MIDI_QueueByte(uint8_t b)
+{
+    fifo[fifoWr] = b;
+    fifoWr = (fifoWr + 1) % BUFFER_SIZE;
+}
+
