ad6d104879718b227c2922c7467c83d151380c43
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2014-09-02 16:40:09 +0200

    Panel code cleaned

--- firmware/panel.h
+++ firmware/panel.h
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2014, Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
+ * All rights reserved.
+ *
+ * This file is distributed under the Modified BSD License.
+ * See LICENSE.txt for details.
+ */
+
+/*
+ * This module implements the front panel interaction logic, i.e.
+ * it reacts to key presses and updates settings accordingly, as well
+ * as controls the display contents.
+ */
+
 #ifndef __PANEL_H__
 #define __PANEL_H__
 
