cc2c7b69b00ec35de07c23ff574a9f6837292321
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2014-09-12 14:30:48 +0200

    Get rid of original projectconfig.h

---
+++ firmware/src/projectconfig.h
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2014, Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
+ * All rights reserved.
+ *
+ * This file is distributed under the Modified BSD License.
+ * See LICENSE.txt for details.
+ */
+
+/* Code borrowed from LPC1343CodeBase needs this.
+ * This file is included nu lpc134x.h
+ */
+
+#define CFG_CPU_CCLK    (72000000)
